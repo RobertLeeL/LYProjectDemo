404411ac9823fb6b1568ace17c2c069688a877d4
robertleel <994547393@qq.com>
2018-12-13 21:26:20 +0800

    Initial Commit

---
+++ LYProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYProjectDemo
+//
+//  Created by 李龙跃 on 2018/12/13.
+//  Copyright © 2018 Longyue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYProjectDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LYProjectDemo
+//
+//  Created by 李龙跃 on 2018/12/13.
+//  Copyright © 2018 Longyue Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
